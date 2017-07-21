45a05992dbd37c66553745d61a2b91381ce0c174
Aryaz <sharad.aryaz@gmail.com>
2017-07-21 19:52:19 -0400

    Read tokens from standard input and store in linked list

---
+++ Lexer Analysis/listexp.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+ 
+int main()
+{
+ struct node
+ {
+  int num;
+  struct node *ptr;
+ };
+ 
+ typedef struct node NODE;
+ 
+ NODE *head, *first, *temp=0;
+ int count = 0;
+ int choice = 1;
+ first = 0;
+ 
+ while(choice)
+ {
+  head =(NODE*) malloc(sizeof(NODE));
+  printf("Enter the data item\n");
+  scanf("%d", &head-> num);
+ 
+  if(first != 0)
+  {
+   temp->ptr = head;
+   temp = head;
+  }
+  else
+  {
+   first = temp = head;
+  }
+  fflush(stdin);
+  printf("Do you want to continue(Type 0 or 1)?\n");
+  scanf("%d", &choice);
+ 
+ }         /* End of while */
+ 
+ temp->ptr = 0;
+ temp = first;  /* reset temp to the beginning*/
+ printf("\nstatus of the linked list is\n");
+ 
+ while(temp!=0)
+ {
+  printf("%d=>", temp->num);
+  count++;
+  temp = temp -> ptr;
+ }
+ printf("NULL\n");
+ printf("No. of nodes in the list = %d\n", count);
+ return 0;
+}  /* End of main*/
